9bbbdc5fcb4837b612b63da6b70d00e64edd1c2d
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-16 20:41:10 +0900

    Empty KB controller

---
+++ src/main/java/jp/ac/nii/prl/mape/kb/controller/KBController.java
@@ -0,0 +1,12 @@
+package jp.ac.nii.prl.mape.kb.controller;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Component
+@RequestMapping("/kb")
+public class KBController {
+
+}
