25ed161e6ca37f490e67678764fc00a914b6e3dd
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-16 23:46:59 +0900

    Run get transformation and return view

--- src/main/java/jp/ac/nii/prl/mape/kb/controller/KBController.java
+++ src/main/java/jp/ac/nii/prl/mape/kb/controller/KBController.java
@@ -1,5 +1,12 @@
 package jp.ac.nii.prl.mape.kb.controller;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,11 +24,43 @@ public class KBController {
 
 	@RequestMapping(value="get/{bx}", method=RequestMethod.GET)
 	public String get(@PathVariable String bx) {
-		return null;
+		String view = null;
+		String cmd = String.format("main get %s %s.json", bx, bx);
+		Process p = null;
+		try {
+			p = Runtime.getRuntime().exec(cmd);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return "ERROR: could not execute transformation";
+		}
+		try {
+			p.waitFor();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			return "ERROR: could not complete transformation";
+		}
+		Path path = Paths.get(String.format("%s.json",  bx));
+		try {
+			List<String> allLines = Files.readAllLines(path);
+			for (String line:allLines)
+				view = view + "\n" + line;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return "ERROR: could not read view";
+		}
+		return view;
 	}
 	
 	@RequestMapping(value="put/{bx}", method=RequestMethod.POST)
 	public ResponseEntity<?> put(@PathVariable String bx, @RequestBody String view) {
+		
+		Path path = Paths.get(String.format("%s.json", bx));
+		try (BufferedWriter writer = Files.newBufferedWriter(path)) {
+		    writer.write(view);
+		} catch (IOException ex) {
+			HttpHeaders httpHeaders = new HttpHeaders();
+			return new ResponseEntity<>(null, httpHeaders, HttpStatus.FORBIDDEN);
+		}
 		
 		// create response
 		HttpHeaders httpHeaders = new HttpHeaders();
