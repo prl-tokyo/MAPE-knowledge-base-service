a27fc377372edc75df29632e15c72e2536b35d81
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-16 18:12:02 +0900

    Import generated Spring boot project

---
+++ src/main/java/jp/ac/nii/prl/mape/kb/MapeKnowledgeBaseServiceApplication.java
@@ -0,0 +1,12 @@
+package jp.ac.nii.prl.mape.kb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapeKnowledgeBaseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapeKnowledgeBaseServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/jp/ac/nii/prl/mape/kb/MapeKnowledgeBaseServiceApplicationTests.java
@@ -0,0 +1,18 @@
+package jp.ac.nii.prl.mape.kb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MapeKnowledgeBaseServiceApplication.class)
+@WebAppConfiguration
+public class MapeKnowledgeBaseServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
