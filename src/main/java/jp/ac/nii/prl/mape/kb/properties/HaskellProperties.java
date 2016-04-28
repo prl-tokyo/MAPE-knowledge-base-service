b3580be1d69d0053d4144a433eca714ca4be0f27
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-28 15:12:11 +0900

    Add source path property

--- src/main/java/jp/ac/nii/prl/mape/kb/properties/HaskellProperties.java
+++ src/main/java/jp/ac/nii/prl/mape/kb/properties/HaskellProperties.java
@@ -1,5 +1,8 @@
 package jp.ac.nii.prl.mape.kb.properties;
 
+import javax.validation.Valid;
+
+import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -7,13 +10,27 @@ import org.springframework.stereotype.Component;
 @Component
 public class HaskellProperties {
 
+	@NotEmpty
+	@Valid
 	private String executable = "";
 
+	@NotEmpty
+	@Valid
+	private String sourcePath;
+	
 	public String getExecutable() {
 		return executable;
 	}
 
+	public String getSourcePath() {
+		return sourcePath;
+	}
+
 	public void setExecutable(String executable) {
 		this.executable = executable;
+	}
+
+	public void setSourcePath(String sourcePath) {
+		this.sourcePath = sourcePath;
 	}
 }
