b8e744705ce1687db362f9107a37435a52eb6f79
Lionel Montrieux <lionel@nii.ac.jp>
2016-05-04 11:02:44 +0900

    TransformationException sends HTTP internal server error

---
+++ src/main/java/jp/ac/nii/prl/mape/kb/RestErrorHandler.java
@@ -0,0 +1,19 @@
+package jp.ac.nii.prl.mape.kb;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import jp.ac.nii.prl.mape.kb.controller.TransformationException;
+
+@ControllerAdvice
+public class RestErrorHandler {
+	
+	@ExceptionHandler(TransformationException.class)
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	public void handleTransformationException(TransformationException ex) {
+		
+	}
+
+}
